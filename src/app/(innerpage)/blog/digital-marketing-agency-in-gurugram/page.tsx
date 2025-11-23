9e829f53849ab3952aac8651dce0aa21ec7cab3a
steamdeck <hardikvaish95@gmail.com>
2025-11-23 19:36:19 +0530

    193623112025

--- src/app/(innerpage)/blog/digital-marketing-agency-in-gurugram/page.tsx
+++ src/app/(innerpage)/blog/digital-marketing-agency-in-gurugram/page.tsx
@@ -4,11 +4,11 @@ import BreadCumb from '../../../Components/Common/BreadCumb';
 import DigitalMarketingAgencyGurugram from '../../../Components/BlogDetails/DigitalMarketingAgencyGurugram';
 
 export const metadata: Metadata = {
-  title: 'Digital Marketing Agency in Gurugram | AT Tech Global',
+  title: 'Digital Marketing Agency in Gurugram | AT Tech Global - Digital Agency',
   description: 'Looking for the best digital marketing agency in Gurugram? AT Tech Global offers comprehensive SEO, PPC, social media marketing, and content marketing services to help your business grow. Get expert digital marketing solutions in Gurugram.',
   keywords: 'digital marketing agency in gurugram, seo services gurugram, ppc advertising gurugram, social media marketing gurugram, digital marketing company gurugram, online marketing agency gurugram',
   openGraph: {
-    title: 'Digital Marketing Agency in Gurugram | AT Tech Global',
+    title: 'Digital Marketing Agency in Gurugram | AT Tech Global - Digital Agency',
     description: 'Expert digital marketing services in Gurugram. SEO, PPC, social media marketing, and more to help your business achieve online success.',
     type: 'article',
     locale: 'en_US',
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,12 @@ const public_sans = Public_Sans({
 export const metadata = {
   title: {
     absolute: '',
-    default: 'AT Tech Global - Digital Agency React NextJS Template',
-    template: '%s | AT Tech Global - Digital Agency React NextJS Template',
+    default: 'AT Tech Global - Digital Agency',
+    template: '%s | AT Tech Global - Digital Agency',
   },
   description: 'AT Tech Global - Digital Agency React NextJS Template',
   openGraph: {
-    title: 'AT Tech Global - Digital Agency React NextJS Template',
+    title: 'AT Tech Global - Digital Agency',
     description: 'AT Tech Global - Digital Agency React NextJS Template',
   },
   robots: {
