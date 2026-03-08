2a035cb9ebb663310825a7063dc4928d3bfc4b63
steamdeck <hardikvaish95@gmail.com>
2026-03-09 04:20:21 +0530

    feat: Add new About, Counter, and CTA components to the website.

--- src/app/Components/Cta/Cta.tsx
+++ src/app/Components/Cta/Cta.tsx
@@ -6,59 +6,59 @@ import Link from 'next/link';
 
 const Cta = () => {
 
-    useEffect(() => {
-        loadBackgroudImages();
-    }, []);
+  useEffect(() => {
+    loadBackgroudImages();
+  }, []);
 
-    return (
-        <section className="cs_bg_filed" data-background="/assets/img/project_bg.png">
-        <div className="cs_height_120 cs_height_lg_80"></div>
-        <div className="container">
-          <div className="cs_project_content position-relative">
-            <div className="cs_project_thumbnail_wrapper cs_center">
-              <div className="cs_project_thumbnail cs_radius_20">
-                <Image src="https://placeholders.io/600/400/website?style=photographic&seed=42" alt="img" width={242} height={261} unoptimized={true} />
-              </div>
-              <div className="cs_project_thumbnail cs_radius_20">
-                <Image src="https://placeholders.io/600/400/mobile%20app?style=photographic&seed=41" alt="img" width={242} height={261} unoptimized={true} />
-              </div>
+  return (
+    <section className="cs_bg_filed" data-background="/assets/img/project_bg.png">
+      <div className="cs_height_120 cs_height_lg_80"></div>
+      <div className="container">
+        <div className="cs_project_content position-relative">
+          <div className="cs_project_thumbnail_wrapper cs_center">
+            <div className="cs_project_thumbnail cs_radius_20">
+              <Image src="https://placeholders.io/600/400/website?style=photographic&seed=42" alt="img" width={242} height={261} unoptimized={true} />
             </div>
-            <div className="cs_project_heading text-center">
-              <h2 className="cs_fs_48 cs_mb_30 text-capitalize wow fadeInDown">Have a project in mind?</h2>
-              <Link href="/contact" aria-label="Click to contact us" className="cs_btn cs_style_1 cs_fs_14 cs_bold text-uppercase wow fadeInUp"><span>CONTACT US</span></Link>
+            <div className="cs_project_thumbnail cs_radius_20">
+              <Image src="https://placeholders.io/600/400/mobile%20app?style=photographic&seed=41" alt="img" width={242} height={261} unoptimized={true} />
             </div>
           </div>
-          <div className="cs_height_120 cs_height_lg_80"></div>
-          <div className="cs_counter_grid_1">
-            <div className="cs_counter_wrapper cs_type_1">
-              <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Years of Experience</p>
-              <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
-                <span data-count-to="14" className="odometer"></span>14+
-              </div>
-            </div>
-            <div className="cs_counter_wrapper cs_type_1">
-              <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Happy Customer</p>
-              <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
-                <span data-count-to="500" className="odometer"></span>500+
-              </div>
-            </div>
-            <div className="cs_counter_wrapper cs_type_1">
-              <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Project Delivered</p>
-              <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
-                <span data-count-to="4" className="odometer"></span>4K+
-              </div>
-            </div>
-            <div className="cs_counter_wrapper cs_type_1">
-              <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Employees</p>
-              <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
-                <span data-count-to="45" className="odometer"></span>45+
-              </div>
-            </div>
+          <div className="cs_project_heading text-center">
+            <h2 className="cs_fs_48 cs_mb_30 text-capitalize wow fadeInDown">Have a project in mind?</h2>
+            <Link href="/contact" aria-label="Click to contact us" className="cs_btn cs_style_1 cs_fs_14 cs_bold text-uppercase wow fadeInUp"><span>CONTACT US</span></Link>
           </div>
         </div>
         <div className="cs_height_120 cs_height_lg_80"></div>
-      </section>
-    );
+        <div className="cs_counter_grid_1">
+          <div className="cs_counter_wrapper cs_type_1">
+            <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Years of Experience</p>
+            <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
+              <span data-count-to="14" className="odometer"></span>14+
+            </div>
+          </div>
+          <div className="cs_counter_wrapper cs_type_1">
+            <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Happy Customers</p>
+            <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
+              <span data-count-to="389" className="odometer"></span>389+
+            </div>
+          </div>
+          <div className="cs_counter_wrapper cs_type_1">
+            <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Projects Delivered</p>
+            <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
+              <span data-count-to="421" className="odometer"></span>421+
+            </div>
+          </div>
+          <div className="cs_counter_wrapper cs_type_1">
+            <p className="cs_counter_title cs_fs_18 cs_semibold cs_mb_3">Employees</p>
+            <div className="cs_counter_number cs_fs_48 cs_bold cs_heading_color">
+              <span data-count-to="49" className="odometer"></span>49+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cs_height_120 cs_height_lg_80"></div>
+    </section>
+  );
 };
 
 export default Cta;
